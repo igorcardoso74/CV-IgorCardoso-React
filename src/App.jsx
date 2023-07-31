d0519773a12f8ade91cffda77acd25814af84087
igorcardoso74 <igorcardosomkt21@gmail.com>
2023-07-31 20:58:36 +0100

    Test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Layout/Header/Header";
 import Footer from "./components/Layout/Footer/Footer";
 
-//import Form from "./components/UI/Forms/Form.js";
+// Import Form from "./components/UI/Forms/Form.js";
 
 //Routes
 import { Outlet } from "react-router-dom";
